70d01dd4e5a8f754baf5475a86303f00356342a9
Bonaldodaquitania <bonaldodaquitania@gmail.com>
2026-06-03 12:13:44 +0200

    fix: wrong split input parameters, non iterable objects.

--- dags/simple_python_operator.py
+++ dags/simple_python_operator.py
@@ -27,36 +27,26 @@ def example_python_flow():
         data = load_iris(as_frame=True)
         X = data["data"]
         y = data["target"]
-        return X, y
+        return tuple(X, y)
 
     @task()
-    def split_data(
-        X:np.ndarray, 
-        y:np.ndarray
-    ) -> Tuple[np.ndarray]:
+    def split_data(data: Tuple) -> Tuple[np.ndarray]:
+        X, y = data
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.3, random_state=42
         )
-        return X_train, X_test, y_train, y_test
+        return tuple(X_train, X_test, y_train, y_test)
 
     @task
-    def preprocess_data(
-        X_train:np.ndarray,
-        X_test:np.ndarray,
-        y_train:np.ndarray, 
-        y_test:np.ndarray
-    ) -> Tuple[np.ndarray]:
+    def preprocess_data(splitted_data) -> Tuple[np.ndarray]:
+        X_train, X_test, y_train, y_test = splitted_data
         # random forest does not require normalization
         # do nothing
-        return X_train, X_test, y_train, y_test
+        return tuple(X_train, X_test, y_train, y_test)
 
     @task()
-    def classify(
-        X_train:np.ndarray,
-        X_test:np.ndarray,
-        y_train:np.ndarray, 
-        y_test:np.ndarray
-    ):
+    def classify(preprocessed_data):
+        X_train, X_test, y_train, y_test = preprocessed_data
         # create logistic regression 
         model = RandomForestClassifier(
             n_estimators=200
@@ -70,10 +60,8 @@ def example_python_flow():
         # task completed
     
     data = load_data()
-    splitted_data = split_data(*data)
-    preprocessed_data = preprocess_data(
-        *splitted_data
-    )
-    classify(*preprocessed_data)
+    splitted_data = split_data(data)
+    preprocessed_data = preprocess_data(splitted_data)
+    classify(preprocessed_data)
 
 example_python_flow()
