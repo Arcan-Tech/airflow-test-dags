44ce66d35d0a100c3a8c06efa651001378dc0821
Bonaldodaquitania <bonaldodaquitania@gmail.com>
2026-06-03 10:07:10 +0200

    fix: fixed DAG with python operator (using task decorator instead).

--- dags/simple_python_operator.py
+++ dags/simple_python_operator.py
@@ -1,39 +1,19 @@
 from datetime import datetime, timedelta
 
 # Operators; we need this to operate!
-from airflow.operators.python_operator import PythonOperator 
+# from airflow.operators.python_operator import PythonOperator 
 
 # The DAG object; we'll need this to instantiate a DAG
-from airflow.sdk import DAG
-# from airflow.sdk import dag, task   # doing things the pythonic way
+# from airflow.sdk import DAG
+from airflow.sdk import dag, task   # doing things the pythonic way
 
+from typing import Tuple
+import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-def classify():
-    data = load_iris(as_frame=True)
-
-    X = data["data"]
-    y = data["target"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
-    )
-
-    # create logistic regression 
-    model = RandomForestClassifier(
-        n_estimators=200
-    )
-    # fit model
-    model.fit(X_train, y_train)
-
-    # evaluate fitted model
-    model.score(X_test, y_test)
-
-    # task completed
-
-with DAG(
+@dag(
     "python tutorial",
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
@@ -59,17 +39,59 @@ with DAG(
     start_date=datetime(2026, 6, 5),
     catchup=False,
     tags=["example"],
-) as dag:
+)
 
-    # t1 is an example of task
-    t1 = PythonOperator(
-        task_id='classify',
-        python_callable=classify
-    )
+def example_python_flow():
+
+    @task()
+    def load_data() -> Tuple[np.ndarray]:
+        data = load_iris(as_frame=True)
+        X = data["data"]
+        y = data["target"]
+        return X, y
+
+    @task()
+    def split_data(X:np.ndarray, y:np.ndarray) -> Tuple[np.ndarray]:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.3, random_state=42
+        )
+        return X_train, X_test, y_train, y_test
+
+    @task
+    def preprocess_data(
+        X_train:np.ndarray,
+        X_test:np.ndarray,
+        y_train:np.ndarray, 
+        y_test:np.ndarray
+    ) -> Tuple[np.ndarray]:
+        # random forest does not require normalization
+        # do nothing
+        return X_train, X_test, y_train, y_test
+
+    @task()
+    def classify(
+        X_train:np.ndarray,
+        X_test:np.ndarray,
+        y_train:np.ndarray, 
+        y_test:np.ndarray
+    ) -> float:
+        # create logistic regression 
+        model = RandomForestClassifier(
+            n_estimators=200
+        )
+        # fit model
+        model.fit(X_train, y_train)
+
+        # evaluate fitted model
+        score = model.score(X_test, y_test)
+        print(f"Score is: {score}")
+        # task completed
     
-    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG; OR
-    dag.doc_md = """
-    This is a documentation placed anywhere
-    """  # otherwise, type it like this
+    data = load_data()
+    X_train, X_test, y_train, y_test = split_data(data)
+    X_train, X_test, y_train, y_test = preprocess_data(
+        X_train, X_test, y_train, y_test
+    )
+    classify(X_train, X_test, y_train, y_test)
 
-    t1
+example_python_flow()
